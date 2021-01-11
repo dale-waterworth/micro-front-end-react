6594474054a5b5ff237e252d1ef4d0230854782b
Dale Waterworth <dale.waterworth@and.digital>
2021-01-11 10:35:40 +0000

    Improve message

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <Header>
-          Hello header from app 1
+          Default content from app 1
       </Header>
     </div>
   );
--- src/Header.tsx
+++ src/Header.tsx
@@ -5,7 +5,7 @@ class Header extends React.Component<{ children: any }> {
         let {children} = this.props;
         return <h1 style={{color: '#0384E2'}}>
             <div style={{color: 'red'}}>This is from app 1</div>
-            <div style={{color: 'red'}}>Below is added from the container</div>
+            <div style={{color: 'red'}}>Below is injected into the component</div>
             {children}</h1>;
     }
 }
